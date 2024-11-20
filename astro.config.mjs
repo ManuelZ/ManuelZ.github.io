28964213e3edcd0a848a809683479fefadf8f449
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-11-20 00:43:49 -0500

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://manuelz.github.io',
+  site: 'https://ManuelZ.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
